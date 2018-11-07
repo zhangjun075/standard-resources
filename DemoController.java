800097f0528ffb4705132211c18648bfce4a6c69
zhangjun075 <zhangjunheling@163.com>
2018-11-07 14:56:18 +0800

    Update DemoController.java

--- DemoController.java
+++ DemoController.java
@@ -1,6 +1,6 @@
-package com.ppdai.tars.web;
+package com.xx.tars.web;
 
-import com.ppdai.tars.service.DemoService;
+import com.xx.tars.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
